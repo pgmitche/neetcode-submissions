4b9f8ae7f032f09372afc00e903b76e76a0c1a9f
Patrick <33585596+pgmitche@users.noreply.github.com>
2026-03-30 16:59:09 +1100

    Add: path-sum - submission-1

---
+++ Data Structures & Algorithms/path-sum/submission-1.go
@@ -0,0 +1,69 @@
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+
+var tracker = &Tracker{stack: make([]int, 0)}
+type Tracker struct {
+	sum int
+	stack []int
+}
+
+func (t *Tracker) backtrack () {
+	// pop
+	var val int
+	val, t.stack = t.stack[len(t.stack)-1], t.stack[:len(t.stack)-1]
+	// recalc
+	t.sum -= val
+}
+
+func (t *Tracker) track (val int) {
+	// push
+	t.stack = append(t.stack, val)
+	// calc
+	t.sum += val
+}
+
+func hasPathSum(root *TreeNode, targetSum int) bool {
+ 	// Create tracker locally without flag
+    tracker := &Tracker{
+        sum: 0,
+        stack:      []int{},
+    }
+
+	var search func( *TreeNode) bool
+	search = func(node *TreeNode) bool {
+		// guard for empty
+		if node == nil {
+			return false
+		}	
+
+		tracker.track(node.Val)
+
+		// if leaf node
+		if node.Left == nil && nil == node.Right {
+			if tracker.sum == targetSum {
+				return true
+			}
+			tracker.backtrack()
+			return false
+		} 
+
+		if search(node.Left) {
+			return true
+		}
+
+		if search(node.Right) {
+			return true
+		}
+
+		tracker.backtrack()
+		return false
+	}
+
+	return search(root)
+}
